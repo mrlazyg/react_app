a7048b12923c1c7b2098e95fe2fdee115afaadd4
Noor Salim <24814579+mrlazyg@users.noreply.github.com>
2022-07-15 18:42:48 +0530

    New Changes

--- src/App.js
+++ src/App.js
@@ -1,89 +1,21 @@
-import React, { Component } from 'react';
-import data from './data.json';
-import classes from './App.css';
-import Persons from './components/Persons/Persons';
+import logo from './logo.svg';
+import './App.css';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    console.log('App.js console');
-  }
-
-  state = data;
-
-  static getDerivedStateFromProps(props, state) {
-    console.log('getDerivedStateFromProps', props);
-    return state;
-  }
-
-  /* switchHandler = (newName) => {
-    // console.log('Clicked!');
-    this.setState({
-      persons: [
-        { name: 'Jane', age: Math.floor(Math.random() * 30) },
-        { name: newName, age: Math.floor(Math.random() * 30) },
-        { name: 'Megha', age: Math.floor(Math.random() * 30) },
-      ],
-    });
-  }; */
-
-  changeHandler = (event, id) => {
-    // console.log('Clicked!');
-    const index = this.state.persons.findIndex((p) => p.id === id);
-    const person = { ...this.state.persons[index] };
-    person.name = event.target.value;
-
-    const persons = [...this.state.persons];
-    persons[index] = person;
-
-    this.setState({ persons: persons });
-  };
-
-  togglePerson = () => {
-    const toggle = this.state.showPerson;
-    this.setState({ showPerson: !toggle });
-  };
-
-  render() {
-    /* const style = {
-      backgroundColor: '#EF7C8E',
-      color: '#EEE',
-      cursor: 'pointer',
-      borderRadius: '5px',
-      padding: '5px 15px',
-      ':hover': {
-        backgroundColor: 'white',
-        color: 'black',
-      },
-    }; */
-
-    let person = null;
-    if (this.state.showPerson) {
-      person = (
-        <div>
-          <Persons persons={this.state.persons} changed={this.changeHandler} />
-        </div>
-      );
-      /* style.backgroundColor = '#025955';
-      style[':hover'] = {
-        backgroundColor: 'black',
-        color: 'white',
-      }; */
-    }
-
-    return (
-      <div className={classes.App}>
-        <h1>Welcome to React</h1>
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload.
         </p>
-        <button className={classes.Button} onClick={this.togglePerson}>
-          Toggle Name
-        </button>
-        {person}
-      </div>
-    );
-  }
+        <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
+
